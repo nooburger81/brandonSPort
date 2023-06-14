dedf4d31761e1dbebd848862952304a90a7cc171
nooburger81 <brandonsebastian@gmail.com>
2023-06-14 16:58:23 -0400

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
                 <p className='text-4xl font-bold inline border-b-4 border-yellow-600 text-gray-300'>
                     Contact
                 </p>
-                <p className='text-gray-300 py-4'>// Submit the form below or shove off</p>
+                <p className='text-gray-300 py-4'>// Submit the form below so we can connect!</p>
             </div>
             <input className='bg-[#ccd6f6] my-4 p-2' type="text" placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
